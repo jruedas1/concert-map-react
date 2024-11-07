5d7235508a0aee65b25888152c677034d8d99944
Javier Ruedas <jruedas@twc.com>
2024-11-07 10:16:04 -0600

    start progress bar

--- src/exploreSearchEventHandlers.js
+++ src/exploreSearchEventHandlers.js
@@ -236,7 +236,6 @@ export const handleConfirmGenreSelection = async (event, map) => {
     for (let i = selectedYear; i < selectedYear+5 && !stopAnimation; i++){
         yearOutputDiv.innerText = i.toString();
         const genreConcertsForSelectedYear = await getConcertsForYearAndGenre(selectedGenreId, i);
-        console.log(genreConcertsForSelectedYear);
         await outputConcertsOnTimer(genreConcertsForSelectedYear['concerts'], map);
     }
 
