d03f34f5dac72110cc745210916684b2660dea13
Javier Ruedas <jruedas@twc.com>
2025-06-04 12:30:15 -0500

    fix missing concert link

--- src/domUtils.js
+++ src/domUtils.js
@@ -394,7 +394,7 @@ export const outputOneVenuesConcertsToPage = venue => {
     const submissionLink = document.createElement('div');
     submissionLink.innerHTML = `
         <h2>Missing a concert?</h2>
-        <a href="https://dev.cedish.utsa.edu/sounds/concert-map-feedback" class="link-btn">Contribute Here</a>
+        <a href="https://cedish.utsa.edu/sounds/concert-map-feedback" class="link-btn">Contribute Here</a>
     `;
     submissionLink.classList.add('submission-link');
     concertsDiv.appendChild(submissionLink);
