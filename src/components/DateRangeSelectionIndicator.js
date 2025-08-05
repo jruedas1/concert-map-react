22a34ea581c31c1892399a1b567bff60657b1940
Javier Ruedas <jruedas@twc.com>
2025-08-05 10:17:46 -0500

    key nav: date range edit

--- src/components/DateRangeSelectionIndicator.js
+++ src/components/DateRangeSelectionIndicator.js
@@ -1,4 +1,11 @@
 function DateRangeSelectionIndicator({ rangeSelected, startYear, endYear, onClick }) {
+    const handleKeyDown = (e) => {
+        if (e.key === "Enter" || e.key === " ") {
+            e.preventDefault();
+            onClick();
+        }
+    };
+
     return (
         <div id="year-range"
              className={`filter ${rangeSelected ? 'explore-selected-filter' : ''}`}
@@ -6,7 +13,13 @@ function DateRangeSelectionIndicator({ rangeSelected, startYear, endYear, onClic
         >
             {!rangeSelected && <h3>SELECT A DATE RANGE</h3>}
             {rangeSelected && <h3>{startYear}-{endYear}</h3>}
-            {rangeSelected && <p className="edit" tabIndex="0">Edit</p>}
+            {rangeSelected && <p
+                className="edit"
+                tabIndex="0"
+                onKeyDown={onClick}
+            >
+                Edit
+            </p>}
         </div>
     );
 }
