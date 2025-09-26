f077a70e3343df9f3e9d05705ebbe154082b41e4
Javier Ruedas <jruedas@twc.com>
2025-09-26 10:02:41 -0500

    search mode selector visibility in mobile

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,5 +1,7 @@
 import '../css/Filters.css';
 import { useContext } from "react";
+import ConcertsContext from "../context/ConcertsContext";
+import ViewportContext from "../context/ViewportContext";
 import SearchModeFilters from "./SearchModeFilters.js";
 import InteractionModeSelector from "./InteractionModeSelector.js";
 import ExploreModeFilters from "./ExploreModeFilters.js";
@@ -8,12 +10,17 @@ function Filters({ onModeSelect, interactionMode }){
     const inSearchMode = interactionMode === 'search';
     const inExploreMode = interactionMode === 'explore';
 
+    const { confirmedYear } = useContext(ConcertsContext);
+    const { isMobile } = useContext(ViewportContext);
+
+    const showSelector = !isMobile || (isMobile && !confirmedYear);
+
     return (
       <section id='filters'>
-          <InteractionModeSelector
+          {showSelector && <InteractionModeSelector
               onModeSelect={onModeSelect}
               interactionMode={interactionMode}
-          />
+          />}
           {inSearchMode && <SearchModeFilters />}
           {inExploreMode && <ExploreModeFilters />}
       </section>
