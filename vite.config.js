5c5588bd89eb699fd0e77f80d9cc12265ed2d513
Javier Ruedas <jruedas@twc.com>
2025-10-29 14:08:55 -0500

    modify deployment info

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/concert-map-react/',
   plugins: [
     {
       name: "treat-js-files-as-jsx",
