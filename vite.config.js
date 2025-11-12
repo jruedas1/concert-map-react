75e9ae569104a9929e1a5e5a425f16943738b860
Javier Ruedas <jruedas@twc.com>
2025-11-12 14:34:05 -0600

    change base path in vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/concert-map-react/',
+  base: './',
   plugins: [
     {
       name: "treat-js-files-as-jsx",
