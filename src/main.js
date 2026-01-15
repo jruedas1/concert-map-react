9863f8dee0425d58dda4cbf8cffd0ed084dbde98
Javier Ruedas <jruedas@twc.com>
2026-01-15 12:19:57 -0600

    restore main.js

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from './App.js';
+import AppProvider from './context/AppProvider.js';
+
+const el = document.getElementById('root');
+const root = ReactDOM.createRoot(el);
+root.render(
+    <AppProvider>
+        <App />
+    </AppProvider>
+);
